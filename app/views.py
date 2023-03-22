afda379773f81476db876e315cc8564b320d609b
familhacker <familhacker@gmail.com>
2023-03-22 15:37:17 +0400

    added test home

--- app/views.py
+++ app/views.py
@@ -1,1 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+
+def home(request):
+    context = {'n': 5}
+
+    return render(request, 'app/home.html', context)
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+let myButton = document.getElementById('myButton');
+let myP = document.getElementById('myP');
+
+myButton.addEventListener('click', () => {
+  myP.innerHTML = '10';
+});
