9f61e5bea40c472a8053ceb5f577a50b2aa168a0
familhacker <familhacker@gmail.com>
2023-03-23 14:36:15 +0400

    added uikit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def home(request):
     context = {'n': 5}
+    if request.method == 'POST':
+        url = request.POST['url']
+        print(url)
+
+        return redirect('home')
 
     return render(request, 'app/home.html', context)
