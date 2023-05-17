427337dc862313136fcd643e1f1086658a65ede6
familhacker <familhacker@gmail.com>
2023-05-17 15:31:36 +0400

    fixed sensitive data exposure

--- app/modules/sensitive_data_exposure.py
+++ app/modules/sensitive_data_exposure.py
@@ -5,19 +5,25 @@ import re
 
 def run(url, scan, paths):
     sensitive_data_patterns = [
-        r'\d{3}-\d{2}-\d{4}',  # SSN
-        r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z|a-z]{2,7}\b',  # Email
-        r'\b4[0-9]{12}(?:[0-9]{3})?\b',  # Visa Card
-        r'\b5[1-5][0-9]{14}\b',  # MasterCard
-        r'\b3[47][0-9]{13}\b',  # American Express Card
-        r'\b6(?:011|5[0-9]{2})[0-9]{12}\b',  # Discover Card
-        r'\b35(?:2[89]|[3-8][0-9])[0-9]{12}\b',  # JCB Card
-        r'\b(?:2131|1800|35\d{3})\d{11}\b',  # JCB Card
-        r'\b[2-9][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01])\b',  # Date of Birth
-        r'\b(?:[0-9]{3}-){2}[0-9]{4}\b',  # Phone number (US format)
-        r'([0-9]{1,3}(\.[0-9]{1,3}){3})',  # IP Address
-        r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b)',  # IP Address with port
-        r'((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,})',  # Strong Password
+        (r'\d{3}-\d{2}-\d{4}', "SSN (Social Security Number)"),
+        (r'\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z|a-z]{2,7}\b', "Email Adress"),
+        (r'\b4[0-9]{12}(?:[0-9]{3})?\b', "Visa Card"),
+        (r'\b5[1-5][0-9]{14}\b', "MasterCard"),
+        (r'\b3[47][0-9]{13}\b', "American Express Card"),
+        (r'\b6(?:011|5[0-9]{2})[0-9]{12}\b', "Discover Card"),
+        (r'\b35(?:2[89]|[3-8][0-9])[0-9]{12}\b', "JCB Card"),
+        (r'\b(?:2131|1800|35\d{3})\d{11}\b', "JCB Card"),
+        (
+            r'\b[2-9][0-9]{3}-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[01])\b',
+            "Date of Birth",
+        ),
+        (r'\b(?:[0-9]{3}-){2}[0-9]{4}\b', "Phone number (US format)"),
+        (r'([0-9]{1,3}(\.[0-9]{1,3}){3})', "IP Address"),
+        (
+            r'(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:\d{1,5}\b)',
+            "IP Address with port",
+        ),
+        (r'((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).{8,})', "Strong Password"),
     ]
 
     response = make_request(url, "GET", scan.auth_detail)
@@ -39,12 +45,12 @@ def run(url, scan, paths):
         response = make_request(full_url, "GET", scan.auth_detail)
         response_text = create_response_text(response)
 
-        for pattern in sensitive_data_patterns:
+        for pattern, description in sensitive_data_patterns:
             if re.search(pattern, response.text):
                 ScanDetail.objects.create(
                     vulnerability_id=6,
                     scan_id=scan.id,
-                    issue=f"Sensitive data might be exposed! Found pattern: {pattern}",
+                    issue=f"Sensitive data might be exposed! Found pattern: {pattern} - {description}",
                     response=response_text,
                     url=full_url,
                 )
